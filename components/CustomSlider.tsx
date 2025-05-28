3848ce84d977d60c3a0cae80d2c28f7632171c90
Levi Murray <levi@levimurray.dev>
2025-05-28 16:46:08 -0400

    slider emojis, step markers, popover animation, continue button

--- components/CustomSlider.tsx
+++ components/CustomSlider.tsx
@@ -1,29 +1,83 @@
 import { Slider } from "@react-native-assets/slider";
 import React from "react";
-import { Image, StyleProp, ViewStyle } from "react-native";
+import { Image, StyleProp, StyleSheet, Text, View, ViewStyle } from "react-native";
 
-export default function CustomSlider({style}: { style?: StyleProp<ViewStyle>}) {
+interface CustomSliderProps {
+    style?: StyleProp<ViewStyle>;
+    onValueChange?: (value: number) => void;
+}
+
+export default function CustomSlider({style, onValueChange}: CustomSliderProps) {
+    const STEP_COUNT = 15;
+    const STEP_MARKER_SKIP_COUNT = 3
+
     return (
         <>
             <Slider
                 style={{ width: "100%", height: 80 }}
                 minimumValue={0}
-                maximumValue={100}
+                maximumValue={STEP_COUNT}
                 minimumTrackTintColor="#fff"
                 maximumTrackTintColor="#7C7AC8"
                 step={1}
-                value={50}
+                value={0}
                 trackStyle={{ borderRadius: 16 }}
                 trackHeight={60}
                 thumbSize={100}
                 thumbStyle={{ backgroundColor: "transparent" }}
+                onValueChange={onValueChange}
                 CustomThumb={({ value }) => (
                     <Image
                         source={require("@/assets/images/slider-thumb-image.png")}
-                        style={{ transform: [{scale: 0.8}] }}
+                        style={styles.customThumb}
                     />
                 )}
-            />
+                StepMarker={(props) => {
+                    // Skip first and last n markers to leave room for emojis
+                    if (props.index < STEP_MARKER_SKIP_COUNT || props.index > STEP_COUNT - STEP_MARKER_SKIP_COUNT) {
+                        return null;
+                    }
+
+                    return (
+                        <View style={styles.stepMarker}></View>
+                    )
+                }}
+            >
+            </Slider>
+            <View style={styles.emojiContainerParent}>
+                <View style={styles.emojiContainer}>
+                    <Text style={styles.emoji}>😩</Text>
+                    <Text style={styles.emoji}>😍</Text>
+                </View>
+            </View>
         </>
     )
 }
+
+const styles = StyleSheet.create({
+    stepMarker: {
+        width: 1,
+        height: 40,
+        backgroundColor: "#9CB0E3",
+    },
+    customThumb: {
+        transform: [{scale: 0.8}],
+        filter: ""
+    },
+    emojiContainerParent: {
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+    },
+    emojiContainer: {
+        width: "95%",
+        height: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: "auto"
+    },
+    emoji: {
+        fontSize: 48,
+    },
+});
--- components/SymptomsModal.tsx
+++ components/SymptomsModal.tsx
@@ -1,47 +1,98 @@
-import React from "react";
-import { Image, Modal, StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Button, Image, Modal, StyleSheet, Text, View } from "react-native";
+import Animated, { useAnimatedStyle, useSharedValue, withDelay, withTiming } from "react-native-reanimated";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CustomSlider from "./CustomSlider";
 import { ThemedText } from "./ThemedText";
 import { ThemedView } from "./ThemedView";
 
-export default function SymptomsModal({
-  visible,
-  onClose,
-}: {
-  visible: boolean;
-  onClose: () => void;
-}) {
-  return (
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={visible}
-      onRequestClose={onClose}
-    >
-        <ThemedView style={{flex: 1}}>
-            <SafeAreaView style={styles.container}>
-                <View style={styles.header}>
-                    <ThemedText style={styles.headerText}>
-                        Let's record your
-                    </ThemedText>
-                    <ThemedText style={{...styles.headerText, ...styles.headerTextSymptoms}}>Symptoms</ThemedText>
-                </View>
-                <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-                    <Image
-                        source={require('@/assets/images/doctor.png')}
-                        style={styles.doctorImage}
-                    />
-                </View>
-                <View style={styles.popover}>
-                    <View style={{}}>
-                        <CustomSlider />
+interface SymptomsModalProps {
+    visible: boolean;
+    onClose: () => void;
+}
+
+export default function SymptomsModal({ visible, onClose }: SymptomsModalProps) {
+    const [canContinueValue, setCanContinueValue] = useState(false);
+    const [popoverVisible, setPopoverVisible] = useState(false);
+    const popoverOffset = useSharedValue(300);
+
+    useEffect(() => {
+        setCanContinueValue(false);
+
+        if (visible && !popoverVisible) {
+            setPopoverVisible(true);
+            return
+        }
+
+    }, [visible])
+
+
+    useEffect(() => {
+        if (popoverVisible) {
+            popoverOffset.value = withDelay(350, withTiming(0, {duration: 400}))
+        } else {
+            popoverOffset.value = withTiming(300, {duration: 300})
+        }
+    }, [popoverVisible])
+
+    const onContinueButtonPress = () => {
+        setCanContinueValue(false);
+        setPopoverVisible(false);
+        setTimeout(() => {
+            onClose();
+        }, 350);
+    }
+
+    const popoverAnimatedStyle = useAnimatedStyle(() => ({
+        height: "100%",
+        width: "100%",
+        position: "absolute",
+        bottom: 0,
+        transform: [{ translateY: popoverOffset.value }]
+    }))
+
+
+    return (
+        <Modal
+        animationType="slide"
+        transparent={true}
+        visible={visible}
+        onRequestClose={onClose}
+        >
+            <ThemedView style={{flex: 1}}>
+                <SafeAreaView style={styles.container}>
+                    <View style={styles.header}>
+                        <ThemedText style={styles.headerText}>
+                            Let's record your
+                        </ThemedText>
+                        <ThemedText style={{...styles.headerText, ...styles.headerTextSymptoms}}>Symptoms</ThemedText>
                     </View>
-                </View>
-            </SafeAreaView>
-        </ThemedView>
-    </Modal>
-  );
+                    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+                        <Image
+                            source={require('@/assets/images/doctor.png')}
+                            style={styles.doctorImage}
+                        />
+                    </View>
+                    <Animated.View style={popoverAnimatedStyle}>
+                        <View style={styles.popover}>
+                            <ThemedText style={styles.popoverText}>
+                                On a scale from <Text style={{fontWeight: "bold"}}>1–10</Text>, how do you <Text style={{fontWeight: "bold"}}>feel today</Text>?
+                            </ThemedText>
+                            <View style={{}}>
+                                <CustomSlider onValueChange={value => setCanContinueValue(true)} />
+                            </View>
+                            <Button
+                                title="Continue"
+                                color="white"
+                                disabled={!canContinueValue}
+                                onPress={onContinueButtonPress}
+                            />
+                        </View>
+                    </Animated.View>
+                </SafeAreaView>
+            </ThemedView>
+        </Modal>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -67,14 +118,21 @@ const styles = StyleSheet.create({
         transform: [{ scale: 1.5 }],
     },
     popover: {
+        height: "35%",
         position: "absolute",
         bottom: 0,
-        left: 0,
-        right: 0,
-        padding: 16,
-        height: "25%",
+        padding: 32,
+        paddingTop: 24,
         backgroundColor: "#726BC3",
-        borderTopLeftRadius: 16,
-        borderTopRightRadius: 16,
+        borderTopLeftRadius: 36,
+        borderTopRightRadius: 36,
+    },
+    popoverText: {
+        fontSize: 28,
+        lineHeight: 36,
+        marginBottom: 16,
+    },
+    continueButton: {
+        color: "red",
     }
 })
